7debe379009b620067bb6f34f09d884b4af15c36
rafis <rafael.hernandez.ochoa95@gmail.com>
2025-11-15 00:18:48 -0600

    Limpio conflicto en App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ContactCard from "./ContactCard";
 import "./ContactCard.css";
@@ -12,18 +11,3 @@ function App() {
 }
 
 export default App;
-=======
-import React from "react";
-import ContactCard from "./ContactCard";
-import "./ContactCard.css";
-
-function App() {
-  return (
-    <div>
-      <ContactCard />
-    </div>
-  );
-}
-
-export default App;
->>>>>>> a9134fadcc245ca2d107a723ca170d5362b58ce9
