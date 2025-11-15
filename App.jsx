fb2da33f5e78676fd8f43889b88b98ffa5fb608f
rafis <rafael.hernandez.ochoa95@gmail.com>
2025-11-15 01:18:57 -0600

    feat: botones ver y descargar tarjeta en navbar

--- App.jsx
+++ App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import ContactCard from "./ContactCard";
 import "./ContactCard.css";
+import Tarjeta from "./src/Tarjeta";
 
 function CVPage() {
   return (
@@ -26,14 +27,24 @@ function App() {
       document.body.removeChild(link);
     }
   }, []);
+  const handleDescargarTarjeta = () => {
+    const link = document.createElement("a");
+    link.href = "/memo/tarjeta.jpg";
+    link.download = "tarjeta.jpg";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div>
       <nav style={{ marginBottom: "1rem" }}>
-        <Link to="/">Inicio</Link> | <Link to="/cv">Ver CV</Link>
+        <Link to="/">Inicio</Link> | <Link to="/cv">Ver CV</Link> | <Link to="/tarjeta">Ver tarjeta</Link> | <button style={{marginLeft:4, padding:"2px 10px", borderRadius:6, border:"none", background:"#1976d2", color:"#fff", fontWeight:"bold", cursor:"pointer"}} onClick={handleDescargarTarjeta}>Descargar tarjeta</button>
       </nav>
       <Routes>
         <Route path="/" element={<ContactCard />} />
         <Route path="/cv" element={<CVPage />} />
+        <Route path="/tarjeta" element={<Tarjeta />} />
       </Routes>
     </div>
   );
