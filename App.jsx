f44fbd744f5afda0b3e4c878a2a96fd856fb61ce
Ralph <rafael.hernandez.ochoa95@gmail.com>
2025-11-15 00:03:37 -0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ContactCard from "./ContactCard";
+import "./ContactCard.css";
+
+function App() {
+  return (
+    <div>
+      <ContactCard />
+    </div>
+  );
+}
+
+export default App;
