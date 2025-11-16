c3396648718219c89963fc45f38066a8578be334
rafis <rafael.hernandez.ochoa95@gmail.com>
2025-11-16 00:13:11 -0600

    Update build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/memo/',
+  base: '/',
   plugins: [react()],
   publicDir: 'assets', // Copia la carpeta 'assets' desde la raíz al build final
 });
