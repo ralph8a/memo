9569e441953fccacd529e5026f8bb190b5447959
rafis <rafael.hernandez.ochoa95@gmail.com>
2025-11-15 23:28:26 -0600

    Update build for GitHub Pages

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,5 @@
 # Script para build y despliegue en GitHub Pages
-cd C:\react
 npm run build
-Copy-Item .\dist\* C:\memo -Recurse -Force
-cd C:\memo
 git add .
 git commit -m "Update build for GitHub Pages"
 git push
