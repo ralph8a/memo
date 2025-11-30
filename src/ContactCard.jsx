36ff4b5d4bfb6d561c039614170f214bd08bc6c9
rafis <rafael.hernandez.ochoa95@gmail.com>
2025-11-29 18:49:18 -0600

    Replace avatar SVG with real photo

--- src/ContactCard.jsx
+++ src/ContactCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./ContactCard.css";
-import avatarImg from './assets/avatar.svg';
+import avatarImg from './assets/avatar.jpg';
 import { PhoneIcon, MailIcon, LocationIcon, LinkedinIcon, TwitterIcon, CvIcon, DownloadIcon, ShareIcon } from './Icons';
 import { GithubIcon, InstagramIcon } from './SocialIcons';
 import KrauseLogo from './KrauseLogo';
